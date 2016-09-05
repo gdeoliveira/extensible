2b070c90c0732d47556bea26605200354867ef6a
Gabriel de Oliveira <deoliveira.gab@gmail.com>
2016-09-05 02:08:18 -0300

    Swap around Travis configuration.

--- gemfiles/travis.rb
+++ gemfiles/travis.rb
@@ -1,7 +1,7 @@
 source "https://rubygems.org".freeze
 
 gem "bundler".freeze
-gem "codeclimate-test-reporter".freeze
+gem "codeclimate-test-reporter".freeze, "~> 0.4.1".freeze
 gem "json".freeze, "~> 1.8.3".freeze
 gem "rake".freeze, "~> 10.5.0".freeze
 gem "rspec".freeze
