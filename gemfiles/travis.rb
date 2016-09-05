425d780f8ae3410a82d54a472f5e8355686a9828
Gabriel de Oliveira <deoliveira.gab@gmail.com>
2016-09-05 03:23:19 -0300

    Attempt to run an older version of codeclimate.

--- gemfiles/travis.rb
+++ gemfiles/travis.rb
@@ -1,6 +1,6 @@
 source "https://rubygems.org".freeze
 
-gem "codeclimate-test-reporter".freeze, "0.4.1".freeze
+gem "codeclimate-test-reporter".freeze, "~> 0.3.0".freeze
 gem "json".freeze, "~> 1.8.3".freeze
 gem "rake".freeze, "~> 10.5.0".freeze
 gem "rspec".freeze
