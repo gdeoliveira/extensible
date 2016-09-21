2fa2f7a25aa203f19ae0085e1b1f914ae70f0472
Gabriel de Oliveira <deoliveira.gab@gmail.com>
2016-09-21 19:23:51 -0300

    Lock gem versions.

--- gemfiles/travis.rb
+++ gemfiles/travis.rb
@@ -1,7 +1,7 @@
 source "https://rubygems.org".freeze
 
-gem "codeclimate-test-reporter".freeze
-gem "json".freeze
-gem "rake".freeze
-gem "rspec".freeze
-gem "simplecov".freeze
+gem "codeclimate-test-reporter".freeze, "~> 0.6.0".freeze
+gem "json".freeze, "~> 1.8.3".freeze
+gem "rake".freeze, "~> 10.5.0".freeze
+gem "rspec".freeze, "~> 3.5.0".freeze
+gem "simplecov".freeze, "~> 0.12.0".freeze
