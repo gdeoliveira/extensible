94adf6c9f8f0a9c4e821bcb8f48c87448714e600
Gabriel de Oliveira <deoliveira.gab@gmail.com>
2016-09-22 22:51:34 -0300

    Avoid code coverage for Ruby versions below 1.9.

--- gemfiles/travis_1.8.7.rb
+++ gemfiles/travis_1.8.7.rb
@@ -4,6 +4,5 @@ gem "rspec".freeze
 gem "simplecov".freeze
 
 # 1.8.7 specific versions
-gem "codeclimate-test-reporter".freeze, "0.4.1".freeze
 gem "json".freeze, "~> 1.8.3".freeze
 gem "rake".freeze, "~> 10.5.0".freeze
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-require "simplecov" if ENV["COVERAGE_ENABLED"]
+require "simplecov" if ENV["COVERAGE_ENABLED"] && RUBY_VERSION >= "1.9"
 
 lib = File.expand_path("../../lib/", __FILE__)
 $LOAD_PATH.unshift(lib) unless $LOAD_PATH.include?(lib)
