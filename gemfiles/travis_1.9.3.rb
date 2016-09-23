ebad0c6e64d0d46f023503ce3ce0ff9d9d980ff7
Gabriel de Oliveira <deoliveira.gab@gmail.com>
2016-09-22 21:33:54 -0300

    Add specific Gemfile for Ruby 1.9.3 version.

---
+++ gemfiles/travis_1.9.3.rb
@@ -0,0 +1,9 @@
+source "https://rubygems.org".freeze
+
+gem "codeclimate-test-reporter".freeze
+gem "rake".freeze
+gem "rspec".freeze
+gem "simplecov".freeze
+
+# 1.9.3 specific versions
+gem "json".freeze, "~> 1.8.3".freeze
