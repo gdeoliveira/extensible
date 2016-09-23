1d9bbc3b71e55c7f8d15387f4ab0070fab0850fb
Gabriel de Oliveira <deoliveira.gab@gmail.com>
2016-09-22 22:06:03 -0300

    Add specific Gemfile for Ruby version 1.8.7.

--- gemfiles/travis_1.8.7.rb
+++ gemfiles/travis_1.8.7.rb
@@ -4,6 +4,6 @@ gem "codeclimate-test-reporter".freeze
 gem "rspec".freeze
 gem "simplecov".freeze
 
-# 1.9.2 specific versions
+# 1.8.7 specific versions
 gem "json".freeze, "~> 1.8.3".freeze
 gem "rake".freeze, "~> 10.5.0".freeze
