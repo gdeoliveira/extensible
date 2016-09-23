118604ac337fceef9ef7c201c7b3c11420d9e6a6
Gabriel de Oliveira <deoliveira.gab@gmail.com>
2016-09-22 22:28:38 -0300

    Correct `.travis.yml` file and 1.8.7 version gemfile.

--- gemfiles/travis_1.9.2.rb
+++ gemfiles/travis_1.9.2.rb
@@ -1,9 +1,9 @@
 source "https://rubygems.org".freeze
 
+gem "codeclimate-test-reporter".freeze
 gem "rspec".freeze
 gem "simplecov".freeze
 
-# 1.8.7 specific versions
-gem "codeclimate-test-reporter".freeze, "~> 0.4.1".freeze
+# 1.9.2 specific versions
 gem "json".freeze, "~> 1.8.3".freeze
 gem "rake".freeze, "~> 10.5.0".freeze
