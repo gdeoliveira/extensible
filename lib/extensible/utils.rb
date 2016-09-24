5e0fd2b76182b4b9649df3733d4fd5f881f132c9
Gabriel de Oliveira <deoliveira.gab@gmail.com>
2016-09-24 02:35:38 -0300

    Add documentation to Extensible::Utils.

--- lib/extensible/utils.rb
+++ lib/extensible/utils.rb
@@ -1,13 +1,24 @@
 module Extensible
+  ##
+  # Utils contains utility methods whose implementation depends on the currently used Ruby version.
   module Utils
     class << self
-      if RUBY_VERSION < "1.9".freeze
-        def own_const_defined?(mod, name)
-          mod.const_defined?(name)
+      if RUBY_VERSION >= "1.9".freeze
+        # :nocov:
+
+        ##
+        # call-seq:
+        #   own_const_defined?(mod, const_name) => true or false
+        #
+        # Returns +true+ if constant +const_name+ has been defined in module +mod+ (and not in one of its ancestors).
+        # Returns +false+ otherwise.
+        def own_const_defined?(mod, const_name)
+          mod.const_defined?(const_name, false)
         end
+        # :nocov:
       else
-        def own_const_defined?(mod, name)
-          mod.const_defined?(name, false)
+        def own_const_defined?(mod, const_name)
+          mod.const_defined?(const_name)
         end
       end
     end
