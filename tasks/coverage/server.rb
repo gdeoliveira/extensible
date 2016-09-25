3dcf7f5966ba082710905520a209456b27283536
Gabriel de Oliveira <deoliveira.gab@gmail.com>
2016-09-25 00:00:50 -0300

    Allow to pass port number to Rake tasks that start servers.

--- tasks/coverage/server.rb
+++ tasks/coverage/server.rb
@@ -5,9 +5,9 @@ require "webrick"
 namespace :coverage do
   COVERAGE_SERVER_PORT = 3000
 
-  desc "Start an HTTP server for retrieving coverage reports on port #{COVERAGE_SERVER_PORT}"
-  task :server do
-    server = WEBrick::HTTPServer.new(:DocumentRoot => "coverage/", :Port => COVERAGE_SERVER_PORT)
+  desc "Start an HTTP server for retrieving coverage reports"
+  task :server, [:port] do |_, args|
+    server = WEBrick::HTTPServer.new(:DocumentRoot => "coverage/", :Port => args[:port] || COVERAGE_SERVER_PORT)
     shutdown = proc { server.shutdown }
     %w(INT TERM).each {|signal| trap(signal, &shutdown) }
     server.start
--- tasks/rdoc/server.rb
+++ tasks/rdoc/server.rb
@@ -3,11 +3,11 @@
 require "webrick"
 
 namespace :rdoc do
-  DOCUMENT_SERVER_PORT = 3001
+  DOCUMENT_SERVER_PORT = 3000
 
-  desc "Start an HTTP server for retrieving RDoc documentation files on port #{DOCUMENT_SERVER_PORT}"
-  task :server do
-    server = WEBrick::HTTPServer.new(:DocumentRoot => "doc/", :Port => DOCUMENT_SERVER_PORT)
+  desc "Start an HTTP server for retrieving RDoc documentation files"
+  task :server, [:port] do |_, args|
+    server = WEBrick::HTTPServer.new(:DocumentRoot => "doc/", :Port => args[:port] || DOCUMENT_SERVER_PORT)
     shutdown = proc { server.shutdown }
     %w(INT TERM).each {|signal| trap(signal, &shutdown) }
     server.start
