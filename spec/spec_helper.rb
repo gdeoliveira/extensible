5d2894c8b19da706fd244c3940d23b10409abeb1
Gabriel de Oliveira <deoliveira.gab@gmail.com>
2017-04-17 19:48:47 -0300

    Update `spec_helper.rb` file.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,3 +4,9 @@ require "simplecov" if ENV["COVERAGE_ENABLED"] && RUBY_VERSION >= "1.9"
 
 lib = File.expand_path("../../lib/", __FILE__)
 $LOAD_PATH.unshift(lib) unless $LOAD_PATH.include?(lib)
+
+RSpec.configure do |config|
+  config.mock_with(:rspec) do |mocks|
+    mocks.verify_partial_doubles = true
+  end
+end
